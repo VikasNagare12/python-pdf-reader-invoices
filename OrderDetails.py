f37c88e075e1e0686114db21f6dde437e5dd1083
Vikas Nagare <vikasnagare05@gmail.com>
2021-06-02 13:54:20 +0530

    #4 Update Order Details fto extract Item Name, price, sku,  asin

--- OrderDetails.py
+++ OrderDetails.py
@@ -2,81 +2,80 @@ class order_details(object):
     """[summary]
     """
     def __init__(self) -> None:
-        self.customer_name = ''
-        self.address = ''
-        self.pancard_number=''
-        self.gst_registraction_number=''
-        self.pincode=''
-        self.order_number=''
-        self.order_date=''
-        self.item_description=[]
-        self.item_price=[]
-
-    @property.setter
-    def customer_name(self, value):
-        self.customer_name=value
+        self._customer_name = ''
+        self._address = ''
+        self._pancard_number=''
+        self._gst_registraction_number=''
+        self._pincode=''
+        self._order_number=''
+        self._order_date=''
+        self._item_description=[]
 
     @property
     def customer_name(self):
-        return self.customer_name
+        return self._customer_name
 
-    @property.setter
-    def address(self, value)->None:
-        self.address=value
+    @customer_name.setter
+    def customer_name(self, value):
+        self._customer_name=value
 
     @property
-    def address(self)->str:
-        return self.address
+    def address(self):
+        return self._address
 
-    @property.setter
-    def pancard_number(self, value):
-        self.pancard_number = value
+    @address.setter
+    def address(self, value):
+        self._address=value
 
     @property
     def pancard_number(self):
-        return self.pancard_number
+        return self._pancard_number
 
-    @property.setter
-    def gst_registraction_number(self , value):
-        self.gst_registraction_number = value
+    @pancard_number.setter
+    def pancard_number(self, value):
+        self._pancard_number = value
+
 
     @property
     def gst_registraction_number(self):
-        return self.gst_registraction_number
+        return self._gst_registraction_number
 
-    @property.setter
-    def pincode(self, value):
-        self.pincode = value
+    @gst_registraction_number.setter
+    def gst_registraction_number(self , value):
+        self._gst_registraction_number = value
 
     @property
     def pincode(self):
-        return self.pincode
-    @property.setter
-    def order_date(self,value):
-        self.order_date = value
+        return self._pincode
+
+    @pincode.setter
+    def pincode(self, value):
+        self._pincode = value
+
     @property
     def order_date(self):
-        return self.order_date
+        return self._order_date
 
-    @property.setter
-    def order_number(self,value):
-        self.order_number = value
+    @order_date.setter
+    def order_date(self,value):
+        self._order_date = value
 
     @property
     def order_number(self):
-        return self.order_number
-    @property.setter
-    def item_price(self, value):
-        self.item_price = value
+        return self._order_number
 
-    @property
-    def item_price(self):
-        return self.item_price
+    @order_number.setter
+    def order_number(self,value):
+        self._order_number = value
 
-    @property.setter
-    def item_description(self, value):
-        self.item_description = value
     @property
     def item_description(self):
-        return self.item_description
+        return self._item_description
 
+    @item_description.setter
+    def item_description(self, value):
+        self._item_description = value
+
+    def __str__(self):
+        items = '\n'.join([e.__str__() for e in self.item_description])
+        return self.customer_name+'\n'+self.address+'\n'+self.pancard_number+'\n'+self.gst_registraction_number+'\n'+self.pincode+'\n'+self.order_number+'\n'+self.order_date+' \n'+items+'\n'
